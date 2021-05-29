04af84f24bd829c365638eb02bb6dee279315c00
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-05-29 13:38:04 -0300

    feat: create layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Layout: React.FC = () => {
+    return (
+        <Container>
+            <h2>Layout</h2>
+        </Container>
+    );
+};
+
+export default Layout;
