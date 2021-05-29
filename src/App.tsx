f3a99ee03c2ca8f89a6f0dd9e78bb26c5d78b08c
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-05-29 13:23:33 -0300

    feat: create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 
+import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/dashboard";
 
 const App: React.FC = () => {
     return (
         <>
+            <GlobalStyles />
             <Dashboard />
         </>
     )
