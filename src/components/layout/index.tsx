dcec41f04bbf020b2fb22a843256d0697217f5b2
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-05-29 13:53:36 -0300

    feat: create layout style structure

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Container } from "./styles";
+import Aside from "../aside";
+import Content from "../content";
+import MainHeader from "../main-header";
+import { LayoutGrid } from "./styles";
 
 const Layout: React.FC = () => {
     return (
-        <Container>
-            <h2>Layout</h2>
-        </Container>
+        <LayoutGrid>
+            <MainHeader>
+                
+            </MainHeader>
+            <Aside></Aside>
+            <Content></Content>
+        </LayoutGrid>
     );
 };
 
