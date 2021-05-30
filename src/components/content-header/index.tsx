e002da48e1bcbf4c5930759e6c92b3d092670cc9
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-05-30 15:20:32 -0300

    feat: create content header component structure

---
+++ src/components/content-header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { ContainerHeaderContainer } from "./styles";
+
+const ContentHeader: React.FC = () => {
+    return (<ContainerHeaderContainer>ContentHeader</ContainerHeaderContainer>)
+};
+
+export default ContentHeader;
