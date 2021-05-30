76d8f929ff51f9cee9c2c205f09ebcb36694a513
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-05-30 13:18:21 -0300

    feat: design  main header right side

--- src/components/main-header/index.tsx
+++ src/components/main-header/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { Container } from "./styles";
+import React, {useMemo} from "react";
+import emojis from "../../utils/emojis";
+import { Container, Profile, Welcome, UserName } from "./styles";
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const index = Math.floor(Math.random() * emojis.length);
+        return emojis[index];
+    }, []);
+
     return (
         <Container>
-            <h2>MainHeader</h2>
+            <h2>Toggle</h2>
+
+            <Profile>
+                <Welcome>Olá, {emoji} </Welcome>
+                <UserName>Usuário #1</UserName>
+            </Profile>
         </Container>
     );
 };
