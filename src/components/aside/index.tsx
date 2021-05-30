f0b43cd6bac2fbd01704c3e79cbfc267f3d4c86f
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-05-30 14:27:48 -0300

    feat: design sidebar on aside

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { Container } from "./styles";
+import logoSvg from "../../assets/logo.svg";
+import { MdDashboard, MdArrowUpward, MdArrowDownward, MdExitToApp } from "react-icons/md";
+import { AsideContainer, AsideHeader, LogoImage, Title, MenuContainer, MenuItemLink } from "./styles";
 
 const Aside: React.FC = () => {
     return (
-        <Container>
-            <h2>Aside</h2>
-        </Container>
+        <AsideContainer>
+            <AsideHeader>
+                <LogoImage src={logoSvg} alt="Logo Minha Carteira" />
+                <Title>Minha Carteira</Title>
+            </AsideHeader>
+
+            <MenuContainer>
+                <MenuItemLink href="#" title="Link para o dashboard">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+
+                <MenuItemLink href="#" title="Link para entradas na carteira">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="#" title="Link para saídas na carteira">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+
+                <MenuItemLink href="#" title="Link para sair da aplicação">
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
+        </AsideContainer>
     );
 };
 
--- src/components/aside/styles.ts
+++ src/components/aside/styles.ts
@@ -1,7 +1,45 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const AsideContainer = styled.div`
     grid-area: AS;
     background-color: ${props => props.theme.colors.secondary};
+    padding-left: 20px;
+    border-right: 1px solid ${props => props.theme.colors.gray};
     color: ${props => props.theme.colors.white}
 `;
+export const AsideHeader = styled.header`
+    display: flex;
+    align-items: center;
+    height: 70px;
+`;
+export const LogoImage = styled.img`
+    width: 40px;
+    height: 40px;
+`;
+export const Title = styled.h3`
+    margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;    
+`;
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    transition: opacity .3s;
+    margin: 7px 0;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+        opacity: 0.7;
+    }
+
+    > svg {
+        font-size: x-large;
+        margin-right: 5px;
+    }
+`;
+
