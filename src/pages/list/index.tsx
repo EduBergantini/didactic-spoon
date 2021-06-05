4983605966d3674c0c1147eefb7ba6b8714a5bee
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-06-05 15:47:24 -0300

    refactor: valid hardcoded months and years on list filters

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -5,20 +5,33 @@ import SelectInput from "../../components/select-input";
 import { ListContainer, ListContent } from "./styles";
 
 const Dashboard: React.FC = () => {
-    const options = [
-        {value: 1, label: "Teste 1"},
-        {value: 2, label: "Teste 2"},
-        {value: 3, label: "Teste 3"},
-        {value: 4, label: "Teste 4"},
-        {value: 5, label: "Teste 5"},
-        {value: 6, label: "Teste 6"},
+    const months = [
+        {value: 1, label: "Janeiro"},
+        {value: 2, label: "Fevereiro"},
+        {value: 3, label: "Março"},
+        {value: 4, label: "Abril"},
+        {value: 5, label: "Maio"},
+        {value: 6, label: "Junho"},
+        {value: 7, label: "Julho"},
+        {value: 8, label: "Agosto"},
+        {value: 9, label: "Setembro"},
+        {value: 10, label: "Outubro"},
+        {value: 11, label: "Novembro"},
+        {value: 12, label: "Dezembro"},
     ];
+
+    const years = [
+        {value: 2021, label: "2021"},
+        {value: 2020, label: "2020"},
+        {value: 2019, label: "2019"},
+        {value: 2018, label: "2018"},
+    ]
 
     return (
         <ListContainer>
             <ContentHeader title="Dashboard" lineColor="#e44c4e">
-                <SelectInput options={options}/>
-                <SelectInput options={options}/>
+                <SelectInput options={months}/>
+                <SelectInput options={years}/>
             </ContentHeader>
             <ListContent>
                 <HistoryCard tagColor="#e44c4e" title="Conta de Luz" subtitle="01/01/2001" amount="R$ 79,98" />
