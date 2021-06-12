60b941dbec2c9a86b208abfd8b1808dfc8b9e564
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-06-12 16:20:32 -0300

    refactor: change dashboard header filters to mocked repository data

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,23 +1,53 @@
-import React from "react"
+import React, { useMemo, useState } from "react"
 import ContentHeader from "../../components/content-header";
 import SelectInput from "../../components/select-input";
 import { DashboardContainer } from "./styles";
 
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
+import listOfMonths from "../../utils/months";
+
+
 const Dashboard: React.FC = () => {
-    const options = [
-        {value: 1, label: "Teste 1"},
-        {value: 2, label: "Teste 2"},
-        {value: 3, label: "Teste 3"},
-        {value: 4, label: "Teste 4"},
-        {value: 5, label: "Teste 5"},
-        {value: 6, label: "Teste 6"},
-    ];
+    const currentDate = new Date();
+    const [selectedMonth, setSelectedMonth] = useState<string>((currentDate.getMonth()+1).toLocaleString());
+    const [selectedYear, setSelectedYear] = useState<string>(currentDate.getFullYear().toLocaleString());
+
+    const months = useMemo(() => {
+        return listOfMonths.map((month, index) => {
+                   return {
+                       value: index+1,
+                       label: month
+                   }
+               });
+    }, [])
+
+    const years = useMemo(() => {
+        const yearsWithData: number[] = [];
+        [...gains, ...expenses].forEach(item => {
+            var date = new Date(item.date);
+            var currentItemYear = date.getFullYear();
+
+            if(!yearsWithData.includes(currentItemYear)) {
+                yearsWithData.push(currentItemYear);
+            }
+        });
+
+        setSelectedYear(yearsWithData[yearsWithData.length-1].toString())
+
+        return yearsWithData.map(item => {
+            return {
+                value: item,
+                label: item.toString()
+            }
+        });
+    }, []);
 
     return (
         <DashboardContainer>
             <ContentHeader title="Dashboard" lineColor="#4e41f0">
-                <SelectInput options={options} onSelectInputChange={e => console.log(e.target.value)}/>
-                <SelectInput options={options}  onSelectInputChange={e => console.log(e.target.value)}/>
+            <SelectInput options={months} onSelectInputChange={e => setSelectedMonth(e.target.value)} defaultValue={selectedMonth}/>
+                <SelectInput options={years}  onSelectInputChange={e => setSelectedYear(e.target.value)} defaultValue={selectedYear}/>
             </ContentHeader>
         </DashboardContainer>
     );
