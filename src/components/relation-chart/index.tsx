4dc893b0846604b0dcd54349f4d236819e411583
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-06-13 23:17:47 -0300

    feat: create profit and loss relation graphic component

---
+++ src/components/relation-chart/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { GraphicContainer, InformationContainer, RelationChartContainer, SubTitle, SubtitleContainer, Title } from "./styles";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+
+
+interface IRelationChartProps {
+    data: {
+        name: string;
+        value: number;
+        percentage: number;
+        color: string;
+    }[]
+}
+
+const RelationChart: React.FC<IRelationChartProps> = ({ data }) => (
+    <RelationChartContainer>
+        <InformationContainer>
+            <Title>Relação</Title>
+            <SubtitleContainer>
+                {
+                    data.map((item, index) => (
+                        <SubTitle 
+                            key={index}
+                            color={item.color}>
+                            <div>{item.percentage.toFixed(1)}</div>
+                            <span>{item.name}</span>
+                        </SubTitle>
+                    ))
+                }
+            </SubtitleContainer>
+        </InformationContainer>
+
+        <GraphicContainer>
+            <ResponsiveContainer>
+                <PieChart>
+                    <Pie 
+                        data={data}
+                        labelLine={false}
+                        dataKey="percentage">
+                        {
+                            data.map((item, index) => (
+                                <Cell 
+                                    key={index}
+                                    fill={item.color} />
+                            ))
+                        }
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        </GraphicContainer>
+    </RelationChartContainer>
+)
+
+export default RelationChart;
---
+++ src/components/relation-chart/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+interface ILegendProps {
+    color: string;
+}
+
+export const RelationChartContainer = styled.div`
+    width: 48%;
+    height: 260px;
+    margin: 10px 0;
+    background-color: ${props => props.theme.colors.tertiary};
+    color: ${props => props.theme.colors.white};
+    border-radius: 7px;
+    display: flex;
+`;
+export const InformationContainer = styled.aside`
+    padding: 30px 20px;
+`;
+export const Title = styled.h2`
+    margin-bottom: 20px;
+`;
+export const SubtitleContainer = styled.ul`
+    list-style: none;
+
+    max-height: 175px;
+    overflow-y: scroll;
+    padding-right: 15px;
+
+`;
+export const SubTitle = styled.li<ILegendProps>`
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    
+
+    > div {
+        background-color: ${props => props.color};
+        width: 40px;
+        height: 40px;
+        border-radius: 5px;
+        font-size: 18px;
+        line-height: 40px;
+        text-align: center;
+    }
+    > span {
+        margin-left: 7px;
+    }
+`;
+export const GraphicContainer = styled.main`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+`;
