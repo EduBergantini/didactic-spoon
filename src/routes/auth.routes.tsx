0e1e96eae1ef4a71e03f1be5f846c9a35b35f268
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-06-15 12:04:18 -0300

    feat: create authentication route structure

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Switch, Route} from "react-router-dom";
+import SignIn from "../pages/sign-in";
+
+const AuthRoute: React.FC = () => (
+    <Switch>
+        <Route component={SignIn}/>
+    </Switch>
+)
+
+export default AuthRoute;
