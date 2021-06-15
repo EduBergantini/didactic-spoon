768da59858bcb99ac93cf68a0be7ec7e69c4d726
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-06-15 20:27:36 -0300

    refactor(main-header): make title text smaller for mobile devices

--- src/components/content-header/styles.ts
+++ src/components/content-header/styles.ts
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 
+interface ITitleProps {
+    lineColor: string;
+}
+
 export const ContainerHeaderContainer = styled.div`
     width: 100%;
     display: flex;
     justify-content: space-between;
+
+    @media(max-width: 420px) {
+        flex-direction: column;
+    }
 `;
-interface ITitleProps {
-    lineColor: string;
-}
 export const Title = styled.h2<ITitleProps>`
     &::after {
         content: '';
@@ -15,7 +20,21 @@ export const Title = styled.h2<ITitleProps>`
         width: 55px;
         border-bottom: 10px solid ${props => props.lineColor};
     }
+
+    @media(max-width: 420px) {
+        font-size: 22px;
+        margin-bottom: 15px;
+        &::after {
+            content: '';
+            display: block;
+            width: 55px;
+            border-bottom: 5px solid ${props => props.lineColor};
+        }
+    }
 `;
 export const FiltersControl = styled.div`
     display: flex;
+    @media(max-width: 420px) {
+        margin-bottom: 15px;
+    }
 `;
